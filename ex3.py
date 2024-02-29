25a8e597549438f20b57e9a624494134faeaa47c
Mariem <Mariemouertatani2001@gmail.com>
2024-02-29 13:05:38 -0700

    finished 3

--- ex3.py
+++ ex3.py
@@ -1,6 +1,6 @@
 
 #Question 1: Ai Declaration, I used chargbt to help explain some code lines for me from the list.c file
-#as it was a very big file and hard to understand.
+#as it was a very large file that was hard to understand.
 
 # When resizing an array (lines 59-69), `list_resize` - function used for resizing:, 
 #the new size includes space for the growth of the array that is needed along with additional space. 
@@ -11,48 +11,59 @@
 #This is a dynamic array implementations because a trade-off between memory 
 #usage and  array allocations.
 
-#Ai Declaration - Chatgbt helped me form my answer before as this section 
-#of the exercise was very hard to implement and understand.
+
+#AI generation used for this exercise, Our code was mosty right but there was so much syntax errors that we could not fix
+#so we ran it thorugh chatgbt to fix the bugs and make it functional.
 import sys
 import timeit
 import matplotlib.pyplot as plt
+maximumSize = 80
+experimentRepeats = 1000
+
+def check_list_capacity_changes(maximumSize):
+    sampleList = []
+    initialCapacity = sys.getsizeof(sampleList)
+    for i in range(maximumSize):
+        sampleList.append(i)
+        currentCapacity = sys.getsizeof(sampleList)
+        if currentCapacity != initialCapacity:
+            print(f"Capacity changed: Length {len(sampleList)}, Size {currentCapacity} bytes")
+            initialCapacity = currentCapacity
+
+def measure_growth(size, repetitions):
+    setupCode = f"temp_list = list(range({size}))"
+    testCode = "temp_list.append(None)"
+    times = timeit.repeat(stmt=testCode, setup=setupCode, number=1, repeat=repetitions)
+    return times
 
 
-my_list = []
-oldCapacity = sys.getsizeof(my_list)
+# Check list capacity changes AND measures time
+check_list_capacity_changes(maximumSize)
+growthTime_S_minus_1 = measure_growth(63, experimentRepeats)  
+growthTime_S = measure_growth(80, experimentRepeats)          
 
-for i in range(64):
-    my_list.append(i)
-    newCapacity = sys.getsizeof(my_list)
-    if newCapacity != oldCapacity:
-        print("Capacity changed from", oldCapacity, "bytes to", newCapacity, "bytes when length is", len(my_list))
-        oldCapacity = newCapacity
-
-#  measure time taken to grow array
-measureGrowthTime = lambda change_size, repeats: timeit.repeat(
-    stmt="arr.append(0)", 
-    setup=f"arr = list(range({change_size}))", 
-    number=1, 
-    repeat=repeats
-)
-
-#  parameters
-S = 100000  
-repeats = 1000
-
-# Measure time to grow from S to S+1
-growthTime_from_S_to_Splus1 = measureGrowthTime(S, repeats)
-
-# Measure time to grow from S-1 to S
-growthTime_from_Sminus1_to_S = measureGrowthTime(S - 1, repeats)
-
-# Plotting distributions
+# Plotting 
 plt.figure(figsize=(10, 5))
-plt.hist(growthTime_from_S_to_Splus1, bins=30, alpha=0.5, label='S to S+1')
-plt.hist(growthTime_from_Sminus1_to_S, bins=30, alpha=0.5, label='S-1 to S')
-plt.xlabel('Time to grow array (s)')
+plt.hist(growthTime_S_minus_1, bins=30, alpha=0.7, label='S-1 to S', color='blue')
+plt.hist(growthTime_S, bins=30, alpha=0.7, label='S to S+1', color='red')
+plt.xlabel('Time (seconds)')
 plt.ylabel('Frequency')
-plt.title('Distribution of growth time of array')
+plt.title('Array Growth Time Distribution')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+
+# Plotting distributions
+
+#The distinction between the two measurements is all about how long it takes 
+#to adjust the size of an array. The growth of S to S+1 takes longer than S-1 to S: 
+
+# Array Growth from Size S-1 to S:
+# When the array grows from size S-1 to S, it does so within it's current allocated capacity.
+# Therefore, there will be no resizing needed. - Adding a new element is faster because it's directly put wihtin the current array.
+
+# Array Growth from Size S to S+1:
+# When the array grows from size S to S+1, it usually gets to its current capacity limit.
+# Therefore, the process of adding a new element needs to be done after resizing. This process includes allocating a bigger chunk of memory, 
+#copying all of the already existing elements, and then adding the new ones.
